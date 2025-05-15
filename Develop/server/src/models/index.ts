398e9d091d2d9088a33cb6582a5920ea8080c7eb
atrvvm <annangoctram1@gmail.com>
2025-05-15 06:57:38 -0400

    (feat) Add functionality to authenticateToken method in server/src/middleware/auth.ts

--- Develop/server/src/models/index.ts
+++ Develop/server/src/models/index.ts
@@ -9,6 +9,7 @@ const sequelize = process.env.DB_URL
   ? new Sequelize(process.env.DB_URL)
   : new Sequelize(process.env.DB_NAME || '', process.env.DB_USER || '', process.env.DB_PASSWORD, {
       host: 'localhost',
+      port: 5432,
       dialect: 'postgres',
       dialectOptions: {
         decimalNumbers: true,
